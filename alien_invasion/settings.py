2cb193c260ccec5ae67fcc6c2cf02eb57bed0a76
frankRenlf <frank.renlingfeng@gmail.com>
2023-06-29 09:29:24 +0800

    update

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -21,3 +21,10 @@ class Settings:
         self.bg_color = (230, 230, 230)
 
         self.ship_speed_factor = 0.5
+
+        # 子弹设置
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
+        print(type(self.bullet_color))
